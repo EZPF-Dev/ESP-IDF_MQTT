fde65308d0854e90c14a4e2b147fd948a18892a8
EZPF-Dev <65475056+EZPF-Dev@users.noreply.github.com>
2022-04-02 20:52:48 -0600

    Add files via upload

---
+++ my_data.h
@@ -0,0 +1,10 @@
+#ifndef MY_DATA
+#define MY_DATA
+
+#define SSID "IZZI-E18A"
+#define PASS "9CC8FC90E18A"
+#define URL "mqtt://IoT_HUB:IoT_Devices_are_Awesome@192.168.0.21"
+//#define USER "IoT_HUB"
+//#define PSSW_BROKER "IoT_Devices_are_Awesome"
+
+#endif
