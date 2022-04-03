af1a3954f08f679fc1a107dad4875b3dca8b4aa4
EZPF-Dev <65475056+EZPF-Dev@users.noreply.github.com>
2022-04-02 20:55:37 -0600

    Update my_data.h

--- my_data.h
+++ my_data.h
@@ -1,10 +1,8 @@
 #ifndef MY_DATA
 #define MY_DATA
 
-#define SSID "IZZI-E18A"
-#define PASS "9CC8FC90E18A"
-#define URL "mqtt://IoT_HUB:IoT_Devices_are_Awesome@192.168.0.21"
-//#define USER "IoT_HUB"
-//#define PSSW_BROKER "IoT_Devices_are_Awesome"
+#define SSID "SSID"
+#define PASS "PASS"
+#define URL "mqtt://0.0.0.0:1883"
 
 #endif
